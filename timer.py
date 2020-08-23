640c52ce0fd9909b1125ab04765d1fd936a37293
Tophercwill <45521537+Tophercwill@users.noreply.github.com>
2020-08-23 05:59:47 -0400

    Add files via upload

---
+++ timer.py
@@ -0,0 +1,74 @@
+import discord,random,asyncio,os
+from datetime import datetime
+from discord.ext import commands
+
+send_time='12:00' #time is in 24hr format
+message_channel_id= 715757990870777906 #channel ID to send images to
+
+
+client = discord.Client()
+
+if os.path.isfile('thunderstorm.txt'):
+		with open('thunderstorm.txt', "r") as f:
+			thunder_list = f.read()
+			thunder_list = thunder_list.strip().split("\n")
+
+if os.path.isfile('winds.txt'):
+		with open('winds.txt', "r") as f:
+			wind_list = f.read()
+			wind_list = wind_list.strip().split("\n")
+
+if os.path.isfile('clear.txt'):
+		with open('clear.txt', "r") as f:
+			clear_list = f.read()
+			clear_list = clear_list.strip().split("\n")
+
+if os.path.isfile('clouds.txt'):
+		with open('clouds.txt', "r") as f:
+			cloud_list = f.read()
+			cloud_list = cloud_list.strip().split("\n")
+
+if os.path.isfile('rain.txt'):
+		with open('rain.txt', "r") as f:
+			rain_list = f.read()
+			rain_list = rain_list.strip().split("\n")
+
+if os.path.isfile('special.txt'):
+		with open('special.txt', "r") as f:
+			special_list = f.read()
+			special_list = special_list.strip().split("\n")
+
+@client.event
+async def on_ready():
+	print(client.user.name)
+	print(client.user.id)
+
+async def time_check():
+	await client.wait_until_ready()
+	message_channel= client.get_channel(message_channel_id)
+	while client.is_closed:
+		now=datetime.strftime(datetime.now(),'%H:%M')
+		print(now)
+		if now == send_time:
+			roll = random.randint(1, 100)
+			if roll < 3:
+				message= random.choice(thunder_list)
+			elif roll < 15:
+				message= random.choice(rain_list)
+			elif roll < 55:
+				message= random.choice(cloud_list)
+			elif roll < 80:
+				message= random.choice(clear_list)
+			elif roll < 97:
+				message= random.choice(wind_list)
+			else:
+				message= random.choice(special_list)
+			await message_channel.send(message)
+			time=90
+		else:
+			time=1
+		await asyncio.sleep(time)
+
+client.loop.create_task(time_check())
+
+client.run('NzQ2OTU5NzE1ODE2MTc3Njk0.X0H6cg.f5T5-vQkzoU7hz9BGfvjl1NjvNs')
